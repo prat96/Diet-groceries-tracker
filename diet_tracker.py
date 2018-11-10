3489b27e68e574296659048211ed5eaf66b60737
Pratheek B <pratheek.b@tonboimaging.com>
2018-11-10 17:07:25 +0530

    Initial commit.

---
+++ diet_tracker.py
@@ -0,0 +1,12 @@
+def ingredients():
+    ingredients = [
+        ["skimmed milk", "banana", "rolled oats", "flaxseeds", "peanut butter", "Whey protein", "low fat paneer",
+         "wheat pasta", "almonds", "basmati rice", "chicken breast", "spinach", "potato", "eggs", "brown rice",
+         "veggies"], [700, 1.5, 35, 1, 0.5, 2, 70, 100, 5, 100, 250, 1, 1, 3, 50, 2]]
+
+    print(ingredients[0][0], ingredients[1][0])
+
+
+if __name__ == '__main__':
+    print("Pratheek's diet tracker")
+    ingredients()
