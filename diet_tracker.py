40566bd643cdc69a72f8b0faa22ebee743d365d1
Pratheek B <pratheek.b@tonboimaging.com>
2018-11-10 17:39:06 +0530

    Using files for ingredients/servings.

--- diet_tracker.py
+++ diet_tracker.py
@@ -1,12 +1,16 @@
-def ingredients():
-    ingredients = [
-        ["skimmed milk", "banana", "rolled oats", "flaxseeds", "peanut butter", "Whey protein", "low fat paneer",
-         "wheat pasta", "almonds", "basmati rice", "chicken breast", "spinach", "potato", "eggs", "brown rice",
-         "veggies"], [700, 1.5, 35, 1, 0.5, 2, 70, 100, 5, 100, 250, 1, 1, 3, 50, 2]]
+import numpy as np
 
-    print(ingredients[0][0], ingredients[1][0])
+
+def daily_ingredients():
+    ingredients = [ingredients.rstrip('\n') for ingredients in open('ingredients.txt')]
+    servings = [servings.rstrip('\n') for servings in open('servings.txt')]
+    print(ingredients[0])
+    print(servings[0])
+
+
+
 
 
 if __name__ == '__main__':
     print("Pratheek's diet tracker")
-    ingredients()
+    daily_ingredients()
